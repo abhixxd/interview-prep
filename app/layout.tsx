314849f168bf65edc3c7df5969ebedff065c1a9f
Abhishek <abhishek.188790@gmail.com>
2025-04-16 01:43:23 +0530

    name changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ const monaSans = Mona_Sans({
 });
 
 export const metadata: Metadata = {
-  title: "PrepWise",
+  title: "IntelliCruit",
   description: "An AI-powered platform for preparing for mock interviews",
 };
 
